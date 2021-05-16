286fdfc8008bf1d5cfed2e2419f50f34cb5b7ae5
Johansen512 <tekstogtext@gmail.com>
2021-05-16 12:33:30 +0200

    jumping falling gameOver animation

---
+++ src/scenes/GameOver.js
@@ -0,0 +1,33 @@
+import Phaser from '../lib/phaser.js'
+
+export default class GameOver extends Phaser.Scene
+{
+constructor()
+{
+super('game-over')
+}
+
+create()
+{
+    const width = this.scale.width
+    const height = this.scale.height
+    
+    this.add.text(width * 0.25, height * 0.5, 'Game Over', {
+    fontSize: 48
+    })
+
+    this.add.text(width * 0.5, height * 0.6, 'Press Spacebar to start', {
+        fontSize: 24
+        })
+    .setOrigin(0.5)
+
+
+    this.input.keyboard.once('keydown-SPACE', () => {
+        this.scene.start('game')
+     })
+        
+
+
+
+}
+ }
